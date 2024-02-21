c2431aee00ae9585b9a85c992eb168c93a668801
Tomas Fiala <fialatomas99@gmail.com>
2024-02-21 22:18:29 +0100

    add another transform

--- main.py
+++ main.py
@@ -24,12 +24,20 @@ T_camera_to_robot = np.array([
                         [0, 0, 0, 1]
                             ])
 
+T_camera_to_holder = np.array([
+                        [1, 0, 0, 0],
+                        [0, 1, 0, 0],
+                        [0, 0, 1, 0.8],
+                        [0, 0, 0, 1]
+                            ])
+
 coord_structure.add_transformation('robot', 'world', T_robot_to_world)
 coord_structure.add_transformation('world', 'base', T_world_to_base)
 coord_structure.add_transformation('camera', 'robot', T_camera_to_robot)
+coord_structure.add_transformation('camera', 'holder', T_camera_to_holder)
 
 coord_structure.plot_all('world')
-#coord_structure.draw_diagram()
+coord_structure.draw_diagram()
 
 coord_structure.print_all()
 
